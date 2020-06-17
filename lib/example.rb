fb1d135bf4f2c0140bdd758764c55c3cb6fdb273
Tomas Perez <tomasitopg6@gmail.com>
2020-06-17 04:12:13 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,15 @@
 # Use this space to code along with the readme
+def phrase
+ puts "Hello World!"
+end 
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+phrase
+phrase
+phrase
+phrase
+phrase
+phrase
+phrase
+phrase
+phrase
+phrase
